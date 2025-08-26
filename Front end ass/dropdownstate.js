a78a4516cebe77d0bc3f2e30eb9aa683420ebf29
Hui0112-c <angelahui0112@gmail.com>
2025-08-27 07:53:24 +0800

    Delete Front end ass/dropdownstate.js

--- Front end ass/dropdownstate.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".dropdown > a").forEach(link=> {
-        link.addEventListener("click", e=> {
-                e.preventDefault();
-                const dropdown=link.nextElementSibling;
-                dropdown.style.display=dropdown.style.display==="block" ? "none" : "block";
-            });
-    });
